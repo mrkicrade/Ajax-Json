e8fbd50165fc1b2075f98df57e0bfe8f8b57c4a6
Mrkic Rade <mrkic78@gmail.com>
2019-01-02 12:04:46 +0100

    Ajax & Json

---
+++ main.js
@@ -0,0 +1,34 @@
+let tbody = document.querySelector('tbody');
+
+let xml = new XMLHttpRequest(); 
+
+xml.open('get' , 'http://mysafeinfo.com/api/data?list=bestnovels&format=json');
+xml.addEventListener('readystatechange' , function(){
+    if (xml.readyState == 4 && xml.status == 200) { 
+        display();        
+    }
+})
+xml.send();
+
+
+function display(){
+    console.log(xml.responseText);
+    let data = JSON.parse(xml.responseText);
+    // console.log(data);
+    
+    let text = "";
+    for (let i = 0; i <data.length; i++) {   
+        text += '<tr>';
+        text += '<td>' + i + '</td>';
+        text += '<td>' + data[i].tt + '</td>';
+        text += '<td>' + data[i].au + '</td>';
+        text += '<td>' + data[i].yr + '</td>';
+        text += '<td><a href = "https://en.wikipedia.org/wiki/'+ data[i].tt +'" class="btn btn-primary">Read More</a></td>';
+        text += '</tr>';
+    }
+    tbody.innerHTML = text;
+}
+
+
+
+
